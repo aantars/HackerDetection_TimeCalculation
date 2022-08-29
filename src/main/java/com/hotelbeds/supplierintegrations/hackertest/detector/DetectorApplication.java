d2503724c567ee03a0f9ce92284d65052b77bb4a
Adolfo <aantars@hotmail.com>
2022-08-29 15:07:22 +0200

    Starting the challenge

---
+++ src/main/java/com/hotelbeds/supplierintegrations/hackertest/detector/DetectorApplication.java
@@ -0,0 +1,14 @@
+package com.hotelbeds.supplierintegrations.hackertest.detector;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DetectorApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(DetectorApplication.class, args);
+	}
+
+}
